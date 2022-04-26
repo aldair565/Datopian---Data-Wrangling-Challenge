be936139f29134941ef83361cd1afd37430a483d
Aldair Meraz <aldairrdz@gmail.com>
2022-04-25 21:16:01 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Datopian---Data-Wrangling-Challenge
+# Datopian - Data Wrangling Challenge
+Project made with Nodejs using Axios and Cheerio modules.
+
+## Description
+This script execute a web scraping of the page (Road safety in Europe)[https://en.wikipedia.org/wiki/Road_safety_in_Europe] to search and collect data from table "European Union Road Safety Facts and Figures". Resulting in a CSV file with this information stored in plain text and converted into a xlsx file.
+
+This records are displayed live in a Netlify site and transformed into a pie chart using ChartJs.
+(https://data-wrangling-challenge.netlify.app/)[https://data-wrangling-challenge.netlify.app/]
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+//Get modules
 const axios = require("axios");
 const cheerio = require("cheerio");
-const URL = "https://en.wikipedia.org/wiki/Road_safety_in_Europe";
 const fs = require("fs");
+const URL = "https://en.wikipedia.org/wiki/Road_safety_in_Europe";
 
+//Main async function to do webscraping of page, return all data from table and store it within a CSV file
 async function webScraping(){
+    //Use axios to get page
     const response = await axios.get(URL);
-    const $ = cheerio.load(response.data); //use cheerio load to convert to an object
+
+    //Use cheerio load to convert to an object
+    const $ = cheerio.load(response.data);
     
+    //Declare headers array to store table headers
     const headers = [];
 
+    //Function to get each tag and store the inner text to the headers array
     const tableHead = $("table.wikitable tbody tr th").each((i, el) => {
         if (i <=11){
             headText = $(el).text().trim().replace(/\n/g, "");
@@ -16,32 +23,49 @@ async function webScraping(){
             //console.log(i, headText);
         }
     });
-    headers.splice(6, 1); //remove Road Network Length column
-    headers.splice(-3,3); //remove columns after Total Road Deaths Per Million Inhabitants
-    headers.splice(1, 0, "Year"); //add Year column
+
+    //Remove Road Network Length column
+    headers.splice(6, 1);
+
+    //Remove columns after Total Road Deaths Per Million Inhabitants
+    headers.splice(-3,3);
+
+    //Add Year column
+    headers.splice(1, 0, "Year");
     console.log(headers.join("|"));
 
-    //create csv file with headers delimited by "|" symbol
+    //Create csv file with headers delimited by "|" symbol
     fs.appendFile("European_Union_Road_Safety_Facts_and_Figures.csv", headers.join("|") + "\n", function(err){
         if (err) throw err;
         console.log("File created");
     });
     
+    //Declare allRecords and record arrays to store data of each cell
     const allRecords = [];
     let record = [];
+
+    //Function to get all the data and store the inner text from the table
     const tableData = $("table.wikitable tbody tr td").each((i, el) => {
         if (i <= 318){
             recordText = $(el).text().trim();
             record.push(recordText);
+            
+            //Condition which splits the array in 11 elements
             if (record.length == 11){
-                record.splice(6, 1); //remove Road Network Length column
-                record.splice(-2,2); //remove columns after Total Road Deaths Per Million Inhabitants
-                record.splice(1, 0, "2018"); // add 2018 year
+                record.splice(6, 1);
+                record.splice(-2,2);
+
+                // Add 2018 year
+                record.splice(1, 0, "2018");
+
+                //Writes the record array into the csv file
                 fs.appendFile("European_Union_Road_Safety_Facts_and_Figures.csv", record.join("|") + "\n", function(err){
                     if (err) throw err;
                     console.log("New record added");
                 });
                 console.log(record.join("|"));
+
+                //Appends the record array into allRecords and reset the array
                 allRecords.push(record);
                 record = [];
             }
